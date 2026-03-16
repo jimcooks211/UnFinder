217851c614c51f8fde813e3552063f7ae77ed396
Jimmy Cooks <jimcooks211@gmail.com>
2026-03-16 08:14:21 +0100

    init: UnFinder + deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/UnFind/',   // ← must match your GitHub repo name exactly
+  base: '/UnFinder/',   // ← must match your GitHub repo name exactly
 })
  
